08d9c08843f6cb55bba2f8c64921ae4962d31eb5
theballkyo <theballkyo@gmail.com>
2014-09-24 22:42:05 +0700

    Remove products section in Admin panel.

--- src/app/views/layouts/admin-nav.blade.php
+++ src/app/views/layouts/admin-nav.blade.php
@@ -3,8 +3,6 @@
                             <ul class="widget widget-menu unstyled">
                                 <li class="active"><a href="{{ url('/admin') }}"><i class="menu-icon icon-dashboard"></i>Dashboard
                                 </a></li>
-                                <li><a href="{{url('/admin/products')}}"><i class="menu-icon icon-bullhorn"></i>Products</a>
-                                </li>
                                 <li><a href="{{url('/admin/stocks')}}"><i class="menu-icon icon-inbox"></i>Stocks<b class="label green pull-right">
                                     11</b> </a></li>
                                 <li><a href="{{url('/user/logout')}}"><i class="menu-icon icon-tasks"></i>Logout<b class="label orange pull-right">
